d87f16af820bf101a2f50f54fade52acf750668c
Marynia <marynia@me.com>
2014-06-21 23:21:44 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chimapping::Application.config.secret_token = 'fcb6e8734e0117c3269d8f45a0391f0b690a88adda476b875391f1dfe27e7953b4c965c11d4755d11793b88fe142aac2c2d8e20ab1fdc52e3e7c7a2538d083c8'
