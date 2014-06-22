294310062beffeee65146d7eb86f9f8c45ce9e6c
Marynia <marynia@me.com>
2014-06-22 11:14:43 -0500

    Add Maps Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Chimapping::Application.routes.draw do
   
   root :to => 'pages#home'
   get 'about' => 'pages#about'
+  get 'maps' => 'pages#maps'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
